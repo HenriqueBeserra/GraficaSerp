a274ce41a3d1ac14713b4fee30b035ccc9cc3d7c
HenriqueBeserra <millerhiquinho@gmail.com>
2022-06-26 00:10:17 -0300

    Adaptação da página de contatos para mobile

--- public/revistas/index.js
+++ public/revistas/index.js
@@ -1,8 +1,5 @@
-// const tela = window.screen.width
-const {getImgURL, getEscritor, getRevistas } = require ("../../apiBancodeDados/bdConexão")
-
-
-
+//Muda a página de a cordo com o tamanho da tela, mobile ou pc --> 
+//const tela = window.screen.width
 function changeHref (tela) {
     console.clear()
     if(tela > 600 ) {
@@ -18,12 +15,16 @@ function changeHref (tela) {
     }
     console.log(tela)
 }
-
 // window.document.addEventListener("resize", changeHref(tela) )
+
+
+// Consulta ao banco de dados -->
+const {getImgURL, getEscritor, getRevistas } = require ("../../apiBancodeDados/bdConexão")
 async function teste() {
 
     const bd = await getImgURL()
-    console.log(bd.length)
+    console.log(bd)
 }
+
 console.clear()
-teste()
+//teste()

1be115c48a15a2b44723b5cc758d725d9994c946
HenriqueBeserra <millerhiquinho@gmail.com>
2022-07-04 13:22:54 -0300

    App de monitoração

---
+++ routes/app.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const app = express()
+const port = 3001;
+
+app.use(express.static('../src/public'));
+
+
+app.get("/on", (req, res)=> {
+
+  res.json({mensagem:"Site online at /home/index.html"})
+
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}/home/index.html`)
+})

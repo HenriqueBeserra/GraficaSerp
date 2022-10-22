d55505ab1747f3d93fc5b297d7777ed41add9ff2
HenriqueBeserra <millerhiquinho@gmail.com>
2022-10-22 16:44:56 -0300

    Atualizando Imagens e conteúdo das revistas

--- src/public/revistas/index.js
+++ src/public/revistas/index.js
@@ -3,15 +3,17 @@ const tela = window.screen.width
 function changeHref (tela) {
     console.clear()
     if(tela > 600 ) {
-        let item = document.querySelector("#contatos-revistas")
-        item.href = ("../contatos/contatos.html")
+        let item = document.querySelector("#contatos-revistas");
+        item.href = ("../contatos/contatos.html");
         
-        console.log("Pass")
+        console.log("Pass");
     }
 
     if(tela < 600) {
-        let item = document.querySelector("#contatos-revistas")
-        item.href = ("../contatos/cttMbl.html")
+        let item = document.querySelector("#contatos-revistas");
+        let item2 = document.querySelector("#contatosButton");
+        item.href = ("../contatos/cttMbl.html");
+        item2.href = ("../contatos/cttMbl.html");
     }
     console.log(tela)
 }

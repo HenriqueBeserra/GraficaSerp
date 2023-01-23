f03f45cf59ec7b33b84b6432b071d7124bba0838
HenriqueBeserra <millerhiquinho@gmail.com>
2023-01-23 10:46:50 -0300

    BUG RESOLVIDO, desenvolvimento de volta em andamento

--- src/public/Produtos/api/app.js
+++ src/public/Produtos/api/app.js
@@ -1,28 +1,27 @@
 
-//  import fetch from "node-fetch"
+//import fetch from "node-fetch"
 
 const urlProdutos = "https://apis-erp.vercel.app/app/revistas";
 const urlImg = "https://apis-erp.vercel.app/app/img";
 const ulrEscritor = "https://apis-erp.vercel.app/app/escritor";
 
-const myInit = { method: 'GET',
-               mode: 'no-cors',
-               cache: 'default' };
+const box = document.querySelector(".produtos")
+
 
 
 
 async function getEscritor(id){
 
-    fetch(ulrEscritor, myInit)
+    fetch(ulrEscritor)
     .then( async response => {
         
-        return response.json()
+        return await response.json();
 
     })
     .then( async data => {
 
         
-        console.table(data.informação)
+        console.log(data)
 
     })
 
@@ -31,77 +30,41 @@ async function getEscritor(id){
 
 async function getProdutos(id){
 
-    fetch(urlProdutos, myInit)
+    fetch(urlProdutos)
     .then( async response => {
         
-        return response.json()   
-
+        return await response.json() ;
     })
     .then( async data => {
         
         
-        console.table(data.informação)
+        console.log(data)
         
     })
 
     
 }
 
+
+//Renderiza as imagens na tela
 async function getImage(id){
     
-    fetch(urlImg, myInit)
+    fetch(urlImg)
     .then( async response => {
         
-        return response.json()  
+       return await response.json();
 
     })
     .then( async data => {
         
+        let teste = await data
         
-        console.table(data.informação)
-
+        console.log(teste)
+        
     })
-
     
 }
 
-const produtos = {  
-    
-    teste: () => console.log("Esta exportação deu certo"),
-
-    app: async () =>  {
 
 
-        
-        fetch(urlProdutos, myInit)
-        .then( async response => {
-            
-            return response.json()
-            
-        })
-        .then( async produtos  => {
-            
-            
 
-            let div = document.querySelector('.caixa-com-produto')
-            console.log(div)
-            // let content = produtos.informação;
-            // let quantidade = produtos.informação.length;
-            // const box = document.querySelector('.caxa-com-produto');
-            // const titulo = document.querySelector('.titulo-produtos');
-            // const descrição = document.querySelector('.descrição-produto');        
-            // const img = document.querySelector('.imgProduto')
-
-            // for(let i = 0; i < quantidade; i++){
-                
-            //     titulo.innerHTML = content[i].titulo;
-
-
-            // }
-            
-        } )
-    }
-
-}
-
-getImage()
